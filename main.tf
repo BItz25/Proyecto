df40bb7c50e0cef5dabcee7dc7a1354481db8114
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-18 01:04:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
   }
 
   required_version = ">= 1.2.0"
@@ -21,6 +25,23 @@ resource "aws_instance" "proyecto" {
 
   tags = {
     Name = "proyecto01"
+  }
+}
+
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
   }
 }
   
