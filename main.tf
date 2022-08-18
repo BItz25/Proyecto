fdbdd8aa103bde49fde42e31fcfa115c77407121
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-18 09:28:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,12 +36,5 @@ resource "docker_image" "nginx" {
   keep_locally = false
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
+
   
