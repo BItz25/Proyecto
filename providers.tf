ca104246a5b4369263da2f1fd2d89cfbafd7ea10
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-18 11:02:51 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+  }
+}
+
+
 provider "docker"{
   host="unix:///var/run/docker.sock"
 }
+
+resource "docker_image" "juan" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_conytainer" "index"{
+  name= "index"
+  image = "josuercb/proyecto02"
+  
+  ports{
+    internal=80
+    external=84
+  }
+}
+
+
+
+
+
+
+
