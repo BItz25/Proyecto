53ba32a2db0412616eb10b1c6b775063612cb99a
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-18 10:53:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~>= 2.13.0"
+    }
   }
 
   required_version = ">= 1.2.0"
