3d6939dc8a8e752ae0fd1d900ebb979b3aa1573a
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-19 02:24:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
-    docker = {
+   /* docker = {
       source  = "kreuzwerker/docker"
       version = "2.20.2"
-    }
+    }*/
   }
 
   required_version = ">= 1.2.0"
@@ -35,10 +35,10 @@ output "instance_public_ip" {
 }
 
 
-
+/*
 variable "reponame" {}
 variable "container_port" {}
-/*
+
 provider "docker" {
    host = "unix:///var/run/docker.sock"
 }
